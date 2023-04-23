572bb5156c7a8f984a8314940eb066a0283c7edc
Mubarak <adesinamubarak123@gmail.com>
2023-04-23 23:45:14 +0100

    prepare a database test for the project

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -3,5 +3,5 @@
 -- and with SELECT privilege on performance_schema
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
